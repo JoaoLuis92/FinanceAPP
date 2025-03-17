39cb27142d29216e83d0181bafa2b2b27fc8132d
João Luís Rosa <joaoluis92@gmail.com>
2025-03-17 18:47:27 +0100

    Annual compound calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ results = annual_compound(monthly_deposit, annual_return, total_time, initial_in
 
 chart_data = pd.DataFrame(results[4], columns=["Value"])
 
-st.line_chart(chart_data, x="Time (months)", y="Total value (EUR)")
+st.line_chart(chart_data)
 
 st.write("Value of your investment after ", total_time, " years: ", round(results[0], 0), "€")
 st.write("Total amount invested during this period: ", round(results[1], 0), "€")
