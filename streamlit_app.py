3e82cb9d39debf5fff01e8f706fd033c85a030f1
João Luís Rosa <joaoluis92@gmail.com>
2025-03-17 17:47:01 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Oi Cate')
+
+x = st.slidebar("Rate Cate", 1, 10)
+
+if x == 10:
+    st.write("Yes, cate is a 10")
+else:
+    st.write("Nope, you're mistaken.")
