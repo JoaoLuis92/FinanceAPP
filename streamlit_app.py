8a274bdc5beba44f6e3fe84e36d11fc6ab6bdc0a
João Luís Rosa <joaoluis92@gmail.com>
2025-03-17 18:32:11 +0100

    Annual compound calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,18 +18,24 @@ def annual_compound(monthly_deposit = 100, annual_return = 8, total_time = 10, i
             total_invested += monthly_deposit
     
     total_profit = current_value - total_invested
-    relative_profit = total_profit / total_invested
+    relative_profit = total_profit / total_invested * 100
 
     return (current_value, total_invested, total_profit, relative_profit)
 
+col1, col2 = st.columns(2)
 
-monthly_deposit = st.slider("Monthly deposit in EUR", 0, 1000)
-annual_return = st.slider("Annual return in percentage", 0, 30)
-total_time = st.slider("Total time in years", 1, 50)
-initial_investment = st.slider("Initial investment", 1000, 100000)
+with col1:
+
+    monthly_deposit = st.slider("Monthly deposit in EUR", 0, 1000)
+    annual_return = st.slider("Annual return in percentage", 0, 30)
+
+with col2:
+
+    total_time = st.slider("Total time in years", 1, 50)
+    initial_investment = st.slider("Initial investment", 1000, 100000)
 
 results = annual_compound(monthly_deposit, annual_return, total_time, initial_investment)
 
-st.write("Value of your investment after ", total_time, " years: ", results[0])
-st.write("Total amount invested during this period: ", results[1])
-st.write("This corresponds to a profit of ", results[2], "€, or ", results[3], "%!")
+st.write("Value of your investment after ", total_time, " years: ", round(results[0], 0), "€")
+st.write("Total amount invested during this period: ", round(results[1], 0), "€")
+st.write("This corresponds to a profit of ", round(results[2], 0), "€, or ", round(results[3],0), "%!")
