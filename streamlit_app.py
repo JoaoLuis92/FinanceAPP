a3bdc13e79befefea47185b83c8b10076ed2e038
João Luís Rosa <joaoluis92@gmail.com>
2025-03-20 13:15:49 +0100

    Created library

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,26 +13,29 @@ option_choice = st.selectbox("kunami",("Compound calculators", "others"))
 
 if option_choice == "Compound calculators":
 
-    col1, col2 = st.columns(2)
+    tab1, tab2 = st.tabs(["Annual compound", "Monthly compound"])
 
-    with col1:
+    with tab1:
+        col1, col2 = st.columns(2)
 
-        monthly_deposit = st.slider("Monthly deposit in EUR", 0, 1000)
-        annual_return = st.slider("Annual return in percentage", 0, 30)
+        with col1:
 
-    with col2:
+            monthly_deposit = st.slider("Monthly deposit in EUR", 0, 1000)
+            annual_return = st.slider("Annual return in percentage", 0, 30)
 
-        total_time = st.slider("Total time in years", 1, 50)
-        initial_investment = st.slider("Initial investment", 1000, 100000)
+        with col2:
 
-    results = annual_compound(monthly_deposit, annual_return, total_time, initial_investment)
+            total_time = st.slider("Total time in years", 1, 50)
+            initial_investment = st.slider("Initial investment", 1000, 100000)
 
-    chart_data = pd.DataFrame(results[4], columns=["Value"])
+        results = annual_compound(monthly_deposit, annual_return, total_time, initial_investment)
 
-    st.line_chart(chart_data)
+        chart_data = pd.DataFrame(results[4], columns=["Value"])
 
-    st.metric("Final value", results[0], "{:.2%}".format((results[0] - results[1])/results[1]))
+        st.line_chart(chart_data)
 
-    st.write("Value of your investment after ", total_time, " years: ", round(results[0], 0), "€")
-    st.write("Total amount invested during this period: ", round(results[1], 0), "€")
-    st.write("This corresponds to a profit of ", round(results[2], 0), "€, or ", round(results[3],0), "%!")
+        st.metric("Final value", results[0], "{:.2%}".format((results[0] - results[1])/results[1]))
+
+        st.write("Value of your investment after ", total_time, " years: ", round(results[0], 0), "€")
+        st.write("Total amount invested during this period: ", round(results[1], 0), "€")
+        st.write("This corresponds to a profit of ", round(results[2], 0), "€, or ", round(results[3],0), "%!")
