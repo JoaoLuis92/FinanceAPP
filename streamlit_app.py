cba15bc5611c81511c53cdce7aad77425f04f58f
João Luís Rosa <joaoluis92@gmail.com>
2025-03-20 14:49:09 +0100

    Minor improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 import pandas as pd
 from compound_calculators import *
+from general_functions import *
 
-st.title('Welcome to JL\'s Finance App! :chart_with_upwards_trend:')
-
-st.header("What can you find here?")
-st.write("""
-         I'm a student of Financial Engineering and I am using streamlit to organize the
-         materials I have learned in different subjects of my course. \\
-         Last update: 20/03/2025        
-         """)
+make_title()
 
 # select box
 
