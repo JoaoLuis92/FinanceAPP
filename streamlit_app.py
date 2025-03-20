02d5d6e784dfb4e36db875f2c59b93093ada73fa
João Luís Rosa <joaoluis92@gmail.com>
2025-03-20 13:12:28 +0100

    Created library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from compound_calculators import *
 
 st.title('JL\'s Finance App!')
 
@@ -11,27 +12,6 @@ st.write("Description of what you can find here")
 option_choice = st.selectbox("kunami",("Compound calculators", "others"))
 
 if option_choice == "Compound calculators":
-
-    def annual_compound(monthly_deposit = 100, annual_return = 8, total_time = 10, initial_investment = 1000):
-
-        current_value = initial_investment
-        total_invested = initial_investment
-        data_value = [current_value]
-        data_time = [0]
-
-        for i in range(total_time):
-            for j in range(12):
-
-                current_value = current_value * (1 + annual_return / 100) ** (1/12) + monthly_deposit
-                total_invested += monthly_deposit
-
-                data_value.append(current_value)
-                data_time.append((i + j) / 12)
-        
-        total_profit = current_value - total_invested
-        relative_profit = total_profit / total_invested * 100
-
-        return (current_value, total_invested, total_profit, relative_profit, data_value, data_time)
 
     col1, col2 = st.columns(2)
 
