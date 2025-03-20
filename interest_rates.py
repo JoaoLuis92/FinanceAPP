a5f05ef0db1f8d04ddfacce52bfd8b4eace3649f
João Luís Rosa <joaoluis92@gmail.com>
2025-03-20 16:51:48 +0100

    Added interest rates section

--- interest_rates.py
+++ interest_rates.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from general_functions import *
 
 class InterestRates:
@@ -34,21 +35,29 @@ class InterestRates:
 
     @classmethod
     def simple_rate_block(cls):
-        st.dataframe(cls.simple_rate(1000, 0.08, 10))
 
-    def yearly_compound_block():
+        cls.simple_rate_info()
+        (present_value, interest_rate, number_periods) = cls.simple_rate_inputs()
+        results_dataframe = cls.simple_rate_function(present_value, interest_rate, number_periods)
+        st.dataframe(results_dataframe)
+
+    @classmethod
+    def yearly_compound_block(cls):
         pass
 
-    def periodic_compound_block():
+    @classmethod
+    def periodic_compound_block(cls):
         pass
 
-    def continuous_compound_block():
+    @classmethod
+    def continuous_compound_block(cls):
         pass
 
-    def rate_converter_block():
+    @classmethod
+    def rate_converter_block(cls):
         pass
 
-    # Information blocks
+    # Information about the topics
 
     def interest_rates_info():
         under_development()
@@ -68,19 +77,96 @@ class InterestRates:
     def rate_converter_info():
         under_development()
 
-    # Calculations
+    # Necessary inputs for the functions
 
-    def simple_rate(present_value: float, interest_rate: float, number_periods: int):
+    def simple_rate_inputs():
+        
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            present_value = st.number_input("Initial deposit in EUR", 
+                                            min_value = 0, 
+                                            max_value = 50000,
+                                            value = 1000,
+                                            step = 100)
+        with col2:
+            interest_rate = st.number_input("Interest rate in %", 
+                                            min_value = 0, 
+                                            max_value = 50,
+                                            value = 10,
+                                            step = 1)
+            
+        with col3:
+            number_periods = st.number_input("Number of periods",
+                                             min_value = 1,
+                                             max_value = 50,
+                                             value = 10,
+                                             step = 1)
+            
+        return (present_value, interest_rate, number_periods)
+
+    def yearly_compound_inputs():
+        pass
+
+    def periodic_compound_inputs():
+        pass
+
+    def continuous_compound_inputs():
+        pass
+
+    def rate_converter_inputs():
+        pass
+
+    # Implementation of the mathematical expressions
+
+    def simple_rate_function(present_value: float, interest_rate: float, number_periods: int):
 
         data = [[0, present_value]]
 
-        interest_value = present_value * interest_rate
-
         for i in range(number_periods):
-            present_value += interest_value
-            data.append([i + 1, present_value])
+            data.append([i + 1, present_value * (1 + interest_rate * (i + 1) / 100)])
 
         df = pd.DataFrame(data, columns = ["Time periods", "Value"]).set_index("Time periods")
 
         return df
     
+    def yearly_compound_function(present_value: float, interest_rate: float, number_periods: int):
+    
+        data = [[0, present_value]]
+
+        for i in range(number_periods):
+            data.append([i + 1, present_value * (1 + interest_rate / 100) ** (i + 1)])
+
+        df = pd.DataFrame(data, columns = ["Time periods", "Value"]).set_index("Time periods")
+
+        return df
+    
+    def periodic_compound_function(present_value: float, interest_rate: float, number_periods: int, payment_frequency: int):
+
+        data = [[0, present_value]]
+
+        total_payments = number_periods * payment_frequency
+
+        for i in range(total_payments):
+            data.append([i + 1, present_value * (1 + interest_rate / (payment_frequency * 100)) ** (i + 1)])
+
+        df = pd.DataFrame(data, columns = ["Time periods", "Value"]).set_index("Time periods")
+
+        return df
+    
+    def continuous_compound_function(present_value: float, interest_rate: float, number_periods: int):
+
+        data = [[0, present_value]]
+
+        for i in range(number_periods):
+            data.append([i + 1, present_value * np.exp(interest_rate * (i + 1) / 100)])
+
+        df = pd.DataFrame(data, columns = ["Time periods", "Value"]).set_index("Time periods")
+
+        return df
+    
+    def rate_converter_function(old_interest_rate: float, old_frequency: int, new_frequency: int):
+
+        new_interest_rate = new_frequency * ((1 + old_interest_rate / (old_frequency * 100)) ** (old_frequency / new_frequency) - 1) * 100
+
+        return new_interest_rate
